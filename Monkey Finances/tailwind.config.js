747b795de9ce4b82c7b28d23a42393dc49c7cdd1
FranciscoJSSantos <francisco.jsantana3@gmail.com>
2022-11-15 10:30:16 -0300

    tela de login

--- Monkey Finances/tailwind.config.js
+++ Monkey Finances/tailwind.config.js
@@ -8,11 +8,53 @@ module.exports = {
         content: [
             '!**/{bin,obj,node_modules}/**',
             '**/*.{cshtml,html}',
-            './**/*.razor'
+            './**/*.razor',
+            "./node_modules/flowbite/**/*.js"
         ]
     },
+    darkMode: 'class',
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+            }
+        },
+        fontFamily: {
+            'body': [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ],
+            'sans': [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ]
+        }
     },
     corePlugins: {
         preflight: false,
@@ -20,3 +62,4 @@ module.exports = {
     //prefix: 'tw-',
     plugins: [],
 }
+
